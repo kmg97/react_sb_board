fe4c9447d5a749529222cea8ef81089baba25769
kimmingyu0 <minku4820@gmail.com>
2023-12-06 00:15:37 +0900

    Feat : springboot Dockerfile 추가 및 파일 업로드 경로 변경

--- backend/src/test/java/com/board/BackendApplicationTests.java
+++ backend/src/test/java/com/board/BackendApplicationTests.java
@@ -1,12 +1,13 @@
 package com.board;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-    @Test
+    @Disabled
     void contextLoads() {
     }
 
